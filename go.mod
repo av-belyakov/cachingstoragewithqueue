2bdbbf6e0700c6746c49c5ea70c2adeb439058eb
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-02-04 11:55:07 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/av-belyakov/cachingstoragewithqueue
+
+go 1.23.4
---
+++ interfaces.go
@@ -0,0 +1,17 @@
+package cachingstoragewithqueue
+
+type CacheStorageFuncHandler[T any] interface {
+	CacheStorageGetter[T]
+	CacheStorageSetter[T]
+	Comparison(T) bool
+}
+
+type CacheStorageGetter[T any] interface {
+	GetFunc() func(int) bool
+	GetObject() T
+}
+
+type CacheStorageSetter[T any] interface {
+	SetFunc(func(int) bool)
+	SetObject(T)
+}
